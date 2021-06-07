5f2205c469e800f32e9b4ce5c8f01cf15e6eff72
Ezequiel Faiella <ezequielfaiella@gmail.com>
2021-06-07 13:55:12 -0300

    intentando que grabe

--- app/app/settings.py
+++ app/app/settings.py
@@ -79,9 +79,20 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django',
+#         'USER': 'root',
+#         'PASSWORD': 'example',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
--- app/inv/models.py
+++ app/inv/models.py
@@ -4,14 +4,18 @@ from bases.models import ClaseModelo
 # Create your models here.
 
 class Categoria(ClaseModelo):
+    # el campo id lo crea por defecto django
     descripcion = models.CharField(max_length=100, help_text='Descripción de la Categoría', unique=True)
     
+    # sobreescribo el metodo str para que cambie lo que muestra
     def __str__(self):
         return '{}'.format(self.descripcion)
     
+    # sobreescribo el metodo save para que la descripcion este toda en mayuscula
     def save(self):
         self.descripcion = self.descripcion.upper()
         super(Categoria, self).save
-        
+    
+    # como se va a llamar cuando es plural
     class Meta:
         verbose_name_plural = 'Categorias'
